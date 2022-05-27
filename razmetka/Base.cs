cdab89db932dc72ab5904a4b90d3a4edd30dda9b
unknown <@ngknn.local>
2022-05-27 12:11:17 +0300

    last

---
+++ razmetka/Base.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace razmetka
+{
+    class Base
+    {
+        private static string name;
+        private static string sh1;
+        private static string cash;
+        private static string password;
+
+        public static string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+
+            }
+        }
+        public static string Sh1
+        {
+            get
+            {
+                return sh1;
+            }
+            set
+            {
+                sh1 = value;
+
+            }
+        }
+        public static string Cash
+        {
+            get
+            {
+                return cash;
+            }
+            set
+            {
+                cash = value;
+
+            }
+        }
+        public static string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+
+            }
+        }
+        public static void Registration(string[] DataPlus)
+        {
+            StreamWriter users = new StreamWriter("info.txt", append: true);
+            string line = $"{DataPlus[0]};{DataPlus[1]};{DataPlus[2]}";
+            users.WriteLine(line);
+            users.Close();
+        }
+    }
+}
---
+++ razmetka/Window3.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace razmetka
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        private static bool closing;
+        public Window3()
+        {
+            InitializeComponent();
+            closing = true;
+            MainWindow main_window = new MainWindow();
+        }
+
+        private void Vih_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            closing = false;
+            Close();
+
+        }
+
+        private void Oplata_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            string[] InfoData = new string[3];
+            InfoData[0] = Base.Name;
+            InfoData[1] = Base.Password;
+            InfoData[2] = Convert.ToString(+1);
+            Base.Registration(InfoData);
+            StreamReader students = new StreamReader("info.txt");
+            int i = 0;
+            string login = Log.Text;
+            string password = Pas.Password;
+            string line = students.ReadLine();
+            while (line != null)
+            {
+                string[] splitLine = line.Split(';');
+                string search = splitLine[0];
+                string search1 = splitLine[1];
+                string count = splitLine[2];
+                line = students.ReadLine();
+                if (login.Contains(search) && password.Contains(search1))
+                {
+                    MessageBox.Show("Вы успешно вошли");
+                    Base.Name = Log.Text;
+                    Base.Password = Pas.Password;
+                    Base.Sh1 = count;
+                    i = 1;
+                }
+            }
+            closing = false;
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (closing)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
